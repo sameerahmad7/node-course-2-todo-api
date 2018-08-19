376980a774baa498bce8106300854e0a14ffe84e
Sameer Ahmad <sameer_ahmad324@hotmail.com>
2018-08-19 16:00:28 +0500

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient=require('mongodb').MongoClient;
+const {MongoClient,ObjectID}=require('mongodb');
+
+var obj=new ObjectID();
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err)
+  {
+  return  console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id:new ObjectID('5b794a597da1fca33a82876f')
+  // }).toArray().then((docs)=>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err)=>{
+  //   console.log('Unable to fetch todos',err);
+  // });
+
+  db.collection('Todos').find().count().then((count)=>{
+    console.log(`Todos count: ${count}`);
+
+  },(err)=>{
+    console.log('Unable to fetch todos',err);
+  });
+
+
+//   db.close();
+ });
